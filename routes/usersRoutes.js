128f56e28afaa68c8602f48ad5ba90d8c9fdf672
0926695444 <yo.mohammadpanah@gmail.com>
2026-02-08 22:22:49 +0330

    add express router for users api

---
+++ routes/usersRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const registerValidator = require("../validators/userRegister");
+const usersModel = require("../models/users");
+const { isValidObjectId } = require("mongoose");
+
+const userRouter = express.Router();
+
+userRouter.post("/register", async (req, res) => {
+  const { name, username, email, age, password } = req.body;
+  const validationResult = registerValidator(req.body);
+  if (validationResult !== true) {
+    return res.status(422).json(validationResult);
+  }
+
+  const user = await usersModel.create({
+    name,
+    username,
+    email,
+    age,
+    password,
+  });
+
+  res.status(201).json({
+    message: "New user created successfully",
+    user,
+  });
+});
+
+userRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (isValidObjectId(id)) {
+    const deletedUser = await usersModel.findByIdAndDelete({ _id: id });
+    if (!deletedUser) {
+      res.status(404).json({ message: "There no such user with this id! " });
+    }
+  } else {
+    return res.status(422).json({ message: "User's id is not valid" });
+  }
+
+  res.status(200).json({ message: "User was deleted successfully" });
+});
+
+module.exports = userRouter;
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const usersModel = require("./models/users");
 const booksModel = require("./models/books");
-const registerValidator = require("./validators/userRegister");
 const bookRegisterValidator = require("./validators/booksRegister");
-const { isValidObjectId } = require("mongoose");
+const userRouter = require("./routes/usersRoutes");
 require("./configs/db");
 
 const app = express(); // server
@@ -14,26 +12,7 @@ const app = express(); // server
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post("/api/users/register", async (req, res) => {
-  const { name, username, email, age, password } = req.body;
-  const validationResult = registerValidator(req.body);
-  if (validationResult !== true) {
-    return res.status(422).json(validationResult);
-  }
-
-  const user = await usersModel.create({
-    name,
-    username,
-    email,
-    age,
-    password,
-  });
-
-  res.status(201).json({
-    message: "New user created successfully",
-    user,
-  });
-});
+app.use("/api/users/", userRouter);
 
 app.post("/api/books/register", async (req, res) => {
   const { author, title, price } = req.body;
@@ -53,21 +32,6 @@ app.post("/api/books/register", async (req, res) => {
     message: "New Book was added successfully",
     book,
   });
-});
-
-app.delete("/api/users/:id", async (req, res) => {
-  const { id } = req.params;
-
-  if (isValidObjectId(id)) {
-    const deletedUser = await usersModel.findByIdAndDelete({ _id: id });
-    if (!deletedUser) {
-      res.status(404).json({ message: "There no such user with this id! " });
-    }
-  } else {
-    return res.status(422).json({ message: "User's id is not valid" });
-  }
-
-  res.status(200).json({ message: "User was deleted successfully" });
 });
 
 app.delete("/api/books/:id", async (req, res) => {
